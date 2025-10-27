8ed8a3522245d552fbfeca5b781c1d4b9036ea96
Bassil Saleh <salehbassil42@gmail.com>
2025-10-27 19:20:35 -0400

    Remove commented-out code

--- main.py
+++ main.py
@@ -112,20 +112,6 @@ def validate_args(args):
 		print(f"Message file name: {args.message[0]}")
 		print(f"Public key file name: {args.public_key}")
 		print(f"Private key file name: {args.private_key}")
-		# # Check if the file path exists
-		# if not os.path.exists(args.message[0]):
-		# 	print(f"Error: {args.message[0]} not found.")
-		# 	sys.exit(1)
-		# # Make sure it's a file and not something else like a directory
-		# if not os.path.isfile(args.message[0]):
-		# 	print(f"Error: {args.message[0]} is not a file.")
-		# 	sys.exit(1)
-		# # Open the file and read its contents
-		# print(f"Opening {args.message[0]} ...")
-		# with open(args.message[0]) as message_file:
-		# 	message_contents = message_file.read()
-		# print(f"Contents of {args.message[0]}:")
-		# print(message_contents)
 	elif args.decrypt:
 		if args.message is None:
 			print('Missing argument: -m MESSAGE (or --message MESSAGE)')
@@ -141,52 +127,6 @@ def main():
 	args = cmd_parser.parse_args()
 	print(args)
 	validate_args(args)
-	# print(args)
-	# if args.generate_keypair:
-	# 	if args.new_keypair_name is None:
-	# 		print("When using the option '-n' or '--new-keypair-name', you must specify a name for your key file.")
-	# 		sys.exit(1)
-	# 	print('Generate a keypair.')
-	# 	print(f"New keypair name: {args.new_keypair_name}")
-	# elif args.encrypt:
-	# 	if args.message is None:
-	# 		print('Missing argument: -m MESSAGE (or --message MESSAGE)')
-	# 		print('Where MESSAGE is the file name of your message.')
-	# 		sys.exit(1)
-	# 	if args.public_key is None:
-	# 		print('Missing argument: -p PUBLIC_KEY (or --public-key PUBLIC_KEY)')
-	# 		print('Where PUBLIC_KEY is your public key file.')
-	# 		sys.exit(1)
-	# 	if args.private_key is None:
-	# 		print('Missing argument: -r PRIVATE_KEY (or --private-key PRIVATE_KEY)')
-	# 		print('Where PRIVATE_KEY is your private key file.')
-	# 		sys.exit(1)
-	# 	print('Encrypt a message.')
-	# 	print(f"Message file name: {args.message[0]}")
-	# 	# Check if the file path exists
-	# 	if not os.path.exists(args.message[0]):
-	# 		print(f"Error: {args.message[0]} not found.")
-	# 		sys.exit(1)
-	# 	# Make sure it's a file and not something else like a directory
-	# 	if not os.path.isfile(args.message[0]):
-	# 		print(f"Error: {args.message[0]} is not a file.")
-	# 		sys.exit(1)
-	# 	# Open the file and read its contents
-	# 	print(f"Opening {args.message[0]} ...")
-	# 	with open(args.message[0]) as message_file:
-	# 		message_contents = message_file.read()
-	# 	print(f"Contents of {args.message[0]}:")
-	# 	print(message_contents)
-	# elif args.decrypt:
-	# 	if args.message is None:
-	# 		print('Missing argument: -m MESSAGE (or --message MESSAGE)')
-	# 		print('Where MESSAGE is the file name of your message.')
-	# 		sys.exit(1)
-	# 	print('Decrypt a message.')
-	# 	print(f"Message file name: {args.message[0]}")
-	# else:
-	# 	sys.exit(1)
-	# 	pass
 
 if __name__ == "__main__":
 	main()
